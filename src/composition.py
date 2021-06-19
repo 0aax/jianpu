8baabbb55eb43efcb00ee53e08dcad97c03678e3
Audrey Xie <audrey.hy.xie@gmail.com>
2021-06-19 17:21:42 -0400

    A little edit to composition

--- src/composition.py
+++ src/composition.py
@@ -1,15 +1,14 @@
+from PIL import Image, ImageDraw
+
+DPI = 300 # typical print dpi
+paper_sizes = {'A4': (2480, 3508),
+               'Letter': (2550, 3300)}
+
 class Composition:
-    def __init__(self, title=None, composer=None, instrument=None, \
-                 time=None, key=None, tempo=None):
+    def __init__(self, paper_type, notes):
         
-        self.title = title
-        self.composer = composer
-        self.instrument = instrument
+        self.paper_type = paper_type
+        self.notes = notes
+        
+        self.paper = Image.new('RGB', paper_sizes[paper_type], (255, 255, 255))
 
-        # composition may vary in time/key/tempo, but the 'first' one to appear will be put in the header
-        self.time = time
-        self.key = key
-        self.tempo = tempo
-
-        # self.notes is an array where each element has the format [{'time': abc, 'key': xyz, 'temp': qwe}, [notes....]]
-        self.notes = []
