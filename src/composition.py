0fe7e04c4d3552aeb3736b323b2aa6a4ed38c09a
Audrey Xie <audrey.hy.xie@gmail.com>
2021-06-21 16:27:38 -0400

    Small changes

--- src/composition.py
+++ src/composition.py
@@ -12,3 +12,18 @@ class Composition:
         
         self.paper = Image.new('RGB', paper_sizes[paper_type], (255, 255, 255))
 
+        self.header = {}
+        self.header_height = 0
+
+        self.line_height = 0
+        self.num_lines = 0
+    
+    def set_header_height(self, height):
+        self.header_height = height
+    
+    def set_line_height(self, height):
+        self.line_height = height
+    
+    def set_header(self, header_info):
+        for k, v in header_info.items():
+            self.header[k] = v
