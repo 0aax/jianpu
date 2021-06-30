8ee8e9830f0ed4bb20cd37337d6ee32be943d8ab
Audrey Xie <audrey.hy.xie@gmail.com>
2021-06-30 12:30:03 -0400

    Seeing which sets in config are unused

--- src/config.py
+++ src/config.py
@@ -6,25 +6,25 @@ paper_sizes = {'a4': (2480, 3508),
                'letter': (2550, 3300)}
 
 header_elems = {'title', 'composer', 'instrument', 'affiliation', 'time', 'key', 'tempo'}
-types_measures = {'bar', 'dbar', 'ebar', 'lrep', 'rrep'}
+types_bars = {'bar', 'dbar', 'ebar', 'lrep', 'rrep'}
 
-two_no_add_halloc = {'roct', 'loct'}
-n_no_add_halloc = {'qvr', 'sqvr', 'ccht', 'mm', 'sbrve'}
-no_add_halloc = two_no_add_halloc | n_no_add_halloc | types_measures
+# two_no_add_halloc = {'roct', 'loct'}
+# n_no_add_halloc = {'qvr', 'sqvr', 'ccht', 'mm', 'sbrve'}
+# no_add_halloc = two_no_add_halloc | n_no_add_halloc | types_bars
 
-one_add_halloc_notes = {'down', 'up', 'trem', 'grace', 'scresc', 'ecresc', 'sdim', 'edim'}
-two_add_halloc_notes = {'fing'}
-n_add_halloc_notes = {'chord'}
-add_halloc_notes = one_add_halloc_notes | two_add_halloc_notes | n_add_halloc_notes
+# one_add_halloc_notes = {'down', 'up', 'trem', 'grace', 'scresc', 'ecresc', 'sdim', 'edim'}
+# two_add_halloc_notes = {'fing'}
+# n_add_halloc_notes = {'chord'}
+# add_halloc_notes = one_add_halloc_notes | two_add_halloc_notes | n_add_halloc_notes
 
-two_commands = two_no_add_halloc | two_add_halloc_notes
-n_commands = n_add_halloc_notes | n_no_add_halloc | {'group'}
+# two_commands = two_no_add_halloc | two_add_halloc_notes
+n_commands = {'chord', 'group', 'qvr', 'sqvr', 'ccht', 'mm', 'sbrve'}
 
 pitch = {'roct', 'loct', 'sharp', 'flat', 'natural'}
 
 # note width scaling factors
-note_base_width = 55
-base_spacer = 30
+note_base_width = 30
+base_spacer = int(30/2)
 
 notes_space = {'sqvr': base_spacer*1,
                'qvr': base_spacer*2,
@@ -32,4 +32,14 @@ notes_space = {'sqvr': base_spacer*1,
                'mm': base_spacer*4,
                'sbrve': base_spacer*5}
 
+space_factors = {'sqvr': 1,
+                'qvr': 2,
+                'ccht': 3,
+                'mm': 4,
+                'sbrve': 5}
+
 duration = {'sqvr', 'qvr', 'ccht', 'mm', 'sbrve'}
+
+l_marg, r_marg = 150, 150
+
+elems_notes_line = {'ddot', 'dot', 'sharp', 'flat', 'natural', 'grace', 'fing'} | types_bars
