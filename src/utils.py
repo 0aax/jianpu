87f817e3ec39cf0a629fd64562de74c1ffab86ea
Audrey Xie <audrey.hy.xie@gmail.com>
2021-07-08 11:27:47 -0400

    Renaming and rearranging

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,15 @@ def deep_sum(n):
     """
     if isinstance(n, int): return n
     else: return sum([deep_sum(e) for e in n])
+
+def arr_from_string(string):
+    """
+    Returns an array from a string.
+    """
+    return [e for e in string]
+
+def element_wise_sum(*args):
+    """
+    Given any number of arrays with only strings as elements, returns the element-wise sum of the strings.
+    """
+    return list(map(''.join, zip(*args)))
